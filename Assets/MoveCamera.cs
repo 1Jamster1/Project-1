6d668111914e9fa1b099d14cb92efde1a1a36f2a
Jamster <jasper@live.com.au>
2017-08-04 19:59:09 +1000

    Basic Camera movement implemented

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public float moveSensistivity = 2f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //TODO: Make movement feel better (Currently very sharp)
+
+        //TODO: Move camera with keyboard buttons
+
+        float x = 0f;
+        float y = 0f;
+
+        if (Input.GetMouseButton(0))
+        {
+
+            x = moveSensistivity * Input.GetAxis("Mouse X");
+            y = moveSensistivity * Input.GetAxis("Mouse Y");
+
+            //TODO: Improve logging
+            Debug.Log(x);
+            Debug.Log(y);
+
+            this.transform.Translate(x, y, 0);
+
+        }
+
+	}
+}
